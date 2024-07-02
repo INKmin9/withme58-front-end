89bedd16e696d180e189d64a81af7d67d34acc33
hidanbeing <nd6162bv@gmail.com>
2024-07-02 22:15:34 +0900

    ✨ feat:로그인페이지 제작 시작

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/Landing/LandingPage';
+import LoginPage from './pages/Login/LoginPage';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />}></Route>
+      <Route path="/login" element={<LoginPage />}></Route>
     </Routes>
   );
 }
